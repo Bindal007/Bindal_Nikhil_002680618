435764d1b5443baecc6eadd4ffa06a6fc300624e
Nikhil Bindal <bindal.n@northeastern.edu>
2022-10-26 17:39:25 -0400

    [main branch commit with First assignment]

---
+++ AED_Project_1_Employee_Management/README.md
@@ -0,0 +1,2 @@
+# Bindal_Nikhil_002680618
+AED Assignment (Employee Management System)
---
+++ AED_Project_1_Employee_Management/src/model/ContactDetails.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nikhilbindal
+ */
+public class ContactDetails {
+
+    private String phoneNumber;
+    private String email;
+    private String address;
+    
+
+    public ContactDetails(String phoneNumber, String email, String address) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+    
+    
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
