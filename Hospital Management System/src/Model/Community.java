5adad012bed5aea180901d1163f9d921eb30dccb
Nikhil Bindal <bindal.n@northeastern.edu>
2022-10-30 21:35:49 -0400

    [models for hospital, doctor, patient and their lists updated]

---
+++ Hospital Management System/src/Model/Community.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nikhilbindal
+ */
+public class Community {
+    
+    private String communityName;
+
+    public Community(String communityName) {
+        this.communityName = communityName;
+    }
+
+    public String getCommunityName() {
+        return communityName;
+    }
+
+    public void setCommunityName(String communityName) {
+        this.communityName = communityName;
+    }
+    
+    
+    
+   
+    
+}
